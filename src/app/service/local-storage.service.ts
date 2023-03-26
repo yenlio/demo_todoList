2607cca0882b9d8a284a84ed6f3e7d7ea49e5c14
yenlio <yenliot4311@gmail.com>
2023-03-26 23:53:40 +0700

    tẽc

--- src/app/service/local-storage.service.ts
+++ src/app/service/local-storage.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { TodoModel } from '../model/todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
 
-  constructor() { }
+
+  storage:Storage
+
+  constructor() {
+    this.storage=window.localStorage
+  }
+  setObject(key:string,value:any):void{
+    console.log(key,value,"local");
+
+    if (value!==null){
+      return
+    }
+this.storage[key]=JSON.stringify(value)
+
+  }
+  // getValue (key:any):TodoModel[]{
+  //  return JSON.parse(localStorage.getItem(key)!);
+  // }
+
+  getValue<T>(key:string):T{
+    const obj= JSON.parse(this.storage[key] || null);
+    return <T>obj;
+  }
 }

7d94272a131a0c93eb1de51066add5cf09ec5c72
nguyen yen <you@example.com>
2023-04-03 18:12:44 +0700

    excel data

--- src/app/component/todo-list/todo-list.component.ts
+++ src/app/component/todo-list/todo-list.component.ts
@@ -4,8 +4,11 @@ import {Observable, pipe} from "rxjs";
 import {TodoModel} from "../../model/todo.model";
 import {DataService} from "../../service/data.service";
 import { Subscription } from 'rxjs';
+import { Workbook } from 'exceljs';
+import * as fs from 'file-saver';
 import {map} from "rxjs";
 import {logMessages} from "@angular-devkit/build-angular/src/builders/browser-esbuild/esbuild";
+import {saveAs} from "file-saver";
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -26,18 +29,16 @@ export class TodoListComponent implements OnInit{
     this.todos$=this.service.todos$
 
     // su dung async pipe
-    this.dataApi$=this.data.getData();
-
-
-
+    // this.dataApi$=this.data.getData();
+    // console.log(this.dataApi$," data")
 
     // su dung voi subcribe
-   // this.dataSubscription=this.data.getData().subscribe(
-   //   (item)=>{
-   //     this.dataAPI=item
-   //     console.log(this.dataAPI," dataAPI")
-   //   }
-   // )
+   this.dataSubscription=this.data.getData().subscribe(
+     (item)=>{
+       this.dataAPI=item
+       console.log(this.dataAPI," dataAPI")
+     }
+   )
   }
   ngOnDestroy() {
     this.dataSubscription.unsubscribe();
@@ -51,6 +52,38 @@ export class TodoListComponent implements OnInit{
   delete(e:any){
     console.log(e, "event")
     this.service.deleteItem(e.id)
+  }
+
+  exportExcel(){
+    const title = 'Car Sell Report';
+    const header = ["id", "iduser", "title"]
+
+    let workbook = new Workbook();
+    let worksheet = workbook.addWorksheet('ProductData');
+
+
+
+    worksheet.columns = [
+      { header: 'Id', key: 'id', width: 10 , style: { font: { name: 'Arial Black', size:10} }},
+      { header: 'userId', key: 'userId', width: 32 },
+      { header: 'title', key: 'title', width: 10 },
+    ];
+    worksheet.getCell('B2').value = 5;
+    // Điền dữ liệu vào bảng tính
+    this.dataAPI.forEach((row:any) => {
+      worksheet.addRow(row);
+    });
+    worksheet.addRow([]);
+    // Tự động chỉnh lại chiều rộng của các cột
+    worksheet.columns.forEach((column:any) => {
+      column.width = column.header.length < 15 ? 15 : column.header.length;
+    });
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      saveAs(blob, "lio");
+    });
+
+
   }
 
 
